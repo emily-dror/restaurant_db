6ad6936637c70154993cae250feb528cda820406
Emily Dror <emily.d@campus.technion.ac.il>
2024-12-27 22:06:19 +0200

    Initial Commit: add staff provided files
    Add staff provided documentation, business objects and an example file
    of the solution. In addition, add the requirements to build and run
    the project.

---
+++ README.md
@@ -0,0 +1,19 @@
+# PosgtreSQL based database written in Python
+
+This project describes a delivery management application for a new
+restaurant called "Yummy".
+
+Customers of the app will be able to place orders and rate dishes. Managers of the app will be
+able to add new dishes to the menu, update prices, and track the profits of the restaurant.
+Typically, the data access layer facilitates the interaction of other components of the system
+with the database by providing a simplified API that carries out a predefined desired set of
+operations. A function in the API may receive business objects as Input arguments. These are
+regular Python classes that hold special semantic meaning in the context of the application
+(typically, all other system components are familiar with them). The PDF file that accompanies
+this document contains the set of business objects to be considered in the assignment, as well
+as the full (unimplemented) API.
+
+## The provided files includes:
+1. **Business Objects** – Predefined Python classes with specific semantic meanings.
+2. **API Definition** – A complete, unimplemented set of API functions.
+
---
+++ src/Business/Order.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from typing import Optional
+
+
+class Order:
+    def __init__(self, order_id: Optional[int] = None, date: Optional[datetime] = None,
+                 delivery_fee: Optional[float] = None, delivery_address: Optional[str] = None) -> None:
+        self.__order_id = order_id
+        self.__datetime = date
+        self.__delivery_fee = delivery_fee
+        self.__delivery_address = delivery_address
+
+    def get_order_id(self) -> Optional[int]:
+        return self.__order_id
+
+    def set_order_id(self, order_id: int) -> None:
+        self.__order_id = order_id
+
+    def get_datetime(self) -> Optional[datetime]:
+        return self.__datetime
+
+    def set_datetime(self, date: datetime) -> None:
+        self.__datetime = date
+
+    def get_delivery_fee(self) -> Optional[float]:
+        return self.__delivery_fee
+
+    def set_delivery_fee(self, delivery_fee: float) -> None:
+        self.__delivery_fee = delivery_fee
+
+    def get_delivery_address(self) -> Optional[str]:
+        return self.__delivery_address
+
+    def set_delivery_address(self, delivery_address: str) -> None:
+        self.__delivery_address = delivery_address
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, Order):
+            return False
+        return (
+                self.__order_id == __value.__order_id and
+                self.__datetime == __value.__datetime and
+                self.__delivery_fee == __value.__delivery_fee and
+                self.__delivery_address == __value.__delivery_address
+        )
+
+    def __str__(self) -> str:
+        return (
+            f'order_id={self.__order_id}, date={self.__datetime}, '
+            f'delivery_fee={self.__delivery_fee}, delivery_address={self.__delivery_address}'
+        )
+
+
+class BadOrder(Order):
+    def __init__(self) -> None:
+        super().__init__(order_id=-1, date=datetime.min)
